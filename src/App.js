19057d170d8960c9a6823bb8fe581c7356796344
Farhan <farhankasmani@live.com>
2020-10-04 19:26:39 +1100

    login using GQL

--- src/App.js
+++ src/App.js
@@ -59,36 +59,46 @@ class App extends Component {
   loginHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch(`http://localhost:8080/auth/login`, {
+    const { email, password } = authData;
+    const graphqlQuery = {
+      query: `
+        {
+          login(email: "${email}", password: "${password}") {
+            token
+            userId
+          }
+        }
+      `,
+    };
+    fetch(`http://localhost:8080/graphql`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        email: authData.email,
-        password: authData.password,
-      }),
+      body: JSON.stringify(graphqlQuery),
     })
       .then((res) => {
-        if (res.status === 422) {
-          throw new Error("Validation failed.");
-        }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log("Error!");
-          throw new Error("Could not authenticate you!");
-        }
         return res.json();
       })
       .then((resData) => {
+        const { errors } = resData;
+        if (errors && errors[0].status === 422) {
+          throw new Error("User login failed!");
+        }
+        if (errors) {
+          console.log("Error!");
+          throw new Error("User login failed!");
+        }
         console.log(resData);
+        const { token, userId } = resData.data.login;
         this.setState({
           isAuth: true,
-          token: resData.token,
+          token,
           authLoading: false,
-          userId: resData.userId,
+          userId,
         });
-        localStorage.setItem("token", resData.token);
-        localStorage.setItem("userId", resData.userId);
+        localStorage.setItem("token", token);
+        localStorage.setItem("userId", userId);
         const remainingMilliseconds = 60 * 60 * 1000;
         const expiryDate = new Date(
           new Date().getTime() + remainingMilliseconds
